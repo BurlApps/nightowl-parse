a335dbc66631e65c7ecb22679f714015e11be3d5
Brian Vallelunga <vallelungabrian@gmail.com>
2015-04-01 12:46:55 -0700

    added parse debug accounts

---
+++ cloud/migrations/debugAccounts.js
@@ -0,0 +1,38 @@
+var DebugAccounts = Parse.Object.extend("DebugAccounts")
+
+Parse.Cloud.define("debugAccounts", function(req, res) {
+  Parse.Cloud.useMasterKey()
+
+  var query = new Parse.Query(DebugAccounts)
+  var accounts = {
+    "Debug": [
+      "0bzu64ADBGuHKglFYiCTYKR7tFZVaepLxVT1HXYe",
+      "1VcBDc3O8TogJPqCbiZbfz9zezfI9HaYSHKgRLvG"
+    ],
+    "Brian": [
+      "C46M4EM0yJZmiYKBpgmmoJLRFpgmuPFz8gSPw5A7",
+      "w2cQc3TsNIy6xEVvkS4bt8RVk86A5Bc3NP4SWteL"
+    ],
+    "Nelson": [
+      "AqMUSmYmu4RIUHjCd1QuiL1eN9wG1yt1BZmqFiJ1",
+      "MUPyrGeiJNhb4AdvubKRpzlXYFlXkbsbbgTSWE5F"
+    ]
+  }
+
+  query.each(function(account) {
+    return account.destroy()
+  }).then(function() {
+    for(var key in accounts) {
+      var account = new DebugAccounts()
+
+      account.set("name", key)
+      account.set("appID", accounts[key][0])
+      account.set("appSecret", accounts[key][1])
+      account.save()
+    }
+  }).then(function() {
+    res.success("Successfully added debug accounts")
+  }, function(error) {
+    res.error(error)
+  })
+})
---
+++ cloud/migrations/index.js
@@ -0,0 +1,17 @@
+// Require All Migration Functions
+require("cloud/migrations/debugAccounts")
+
+// Call Migrations
+Parse.Cloud.job("runMigration", function(req, res) {
+  Parse.Cloud.useMasterKey()
+
+  var promise = Parse.Promise.as()
+
+  promise.then(function() {
+    return Parse.Cloud.run("debugAccounts")
+  }).then(function() {
+    res.success("Successfully ran migrations")
+  }, function(error) {
+    res.error(error)
+  })
+})
