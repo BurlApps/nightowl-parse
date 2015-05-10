7646065575c023481290871a16331f30f29e300e
Brian Vallelunga <vallelungabrian@gmail.com>
2015-05-10 14:40:25 -0700

    update ios settings on user change

--- cloud/afterSave/user.js
+++ cloud/afterSave/user.js
@@ -1,5 +1,15 @@
 Parse.Cloud.afterSave(Parse.User, function(req, res) {
   var user = req.object
+  var pushQuery = new Parse.Query(Parse.Installation)
+
+  pushQuery.equalTo("user", user)
+
+  return Parse.Push.send({
+	  where: pushQuery,
+	  data: {
+      action: "settingsController.reload"
+    }
+  })
 
   if(user.existed()) return
 
