cf3918a4db5fa57c533ea5728cc739b07ef78cee
Brian Vallelunga <vallelungabrian@gmail.com>
2015-05-10 15:26:05 -0700

    added self correcting logic

--- cloud/beforeSave/user.js
+++ cloud/beforeSave/user.js
@@ -4,15 +4,15 @@ Parse.Cloud.beforeSave(Parse.User, function(req, res) {
   var user = req.object
 
   Settings().then(function(settings) {
+    user.set("tutoring", !!user.get("tutoring"))
+    user.set("tutorEmail", !!user.get("tutorEmail"))
+    user.set("unsubscribe", !!user.get("unsubscribe"))
+
+    if(!user.get("source")) user.set("source", "ios")
     if(!user.isNew()) return
 
     if(!user.get("charges")) user.set("charges", 0)
     if(!user.get("freeQuestions")) user.set("freeQuestions", settings.get("freeQuestions"))
-    if(!user.get("source")) user.set("source", "ios")
-
-    user.set("tutoring", !!user.get("tutoring"))
-    user.set("tutorEmail", !!user.get("tutorEmail"))
-    user.set("unsubscribe", !!user.get("unsubscribe"))
   }).then(function() {
     res.success()
   }, function(error) {
