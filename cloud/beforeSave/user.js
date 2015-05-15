f950971f2b8a0e39ce59d727b6d6858dca99ec0e
Brian Vallelunga <vallelungabrian@gmail.com>
2015-05-15 01:22:55 -0700

    made new user more effecient

--- cloud/beforeSave/user.js
+++ cloud/beforeSave/user.js
@@ -3,14 +3,14 @@ var Settings = require("cloud/utils/settings")
 Parse.Cloud.beforeSave(Parse.User, function(req, res) {
   var user = req.object
 
+  user.set("tutoring", !!user.get("tutoring"))
+  user.set("tutorEmail", !!user.get("tutorEmail"))
+  user.set("unsubscribe", !!user.get("unsubscribe"))
+
+  if(!user.get("source")) user.set("source", "ios")
+  if(!user.isNew()) return res.success()
+
   Settings().then(function(settings) {
-    user.set("tutoring", !!user.get("tutoring"))
-    user.set("tutorEmail", !!user.get("tutorEmail"))
-    user.set("unsubscribe", !!user.get("unsubscribe"))
-
-    if(!user.get("source")) user.set("source", "ios")
-    if(!user.isNew()) return
-
     if(!user.get("charges")) user.set("charges", 0)
     if(!user.get("freeQuestions")) user.set("freeQuestions", settings.get("freeQuestions"))
   }).then(function() {
