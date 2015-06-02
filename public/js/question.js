f488b7ff85a21d42eafdb492dfd99588360a06a9
Brian Vallelunga <vallelungabrian@gmail.com>
2015-06-01 18:43:28 -0700

    tutors cant get paid more than once per question

--- public/js/question.js
+++ public/js/question.js
@@ -11,17 +11,27 @@ function uploadTriggered() {
   var Assignment = Parse.Object.extend("Assignment")
   var question = new Assignment()
   var file = new Parse.File("image.png", this.files[0])
+  var payTutor = false
 
   question.id = config.question
   $(".good span").text("UPLOADING")
 
   file.save().then(function() {
+    return question.fetch()
+  }).then(function() {
+    payTutor = question.get("state") == 2
+
     question.set("state", 3)
     question.set("answer", file)
     return question.save()
   }).then(function() {
     alert("Question has been answered!")
-    location.href = "/questions/" + question.id + "/answered"
+
+    if(payTutor) {
+      location.href = "/questions/" + question.id + "/answered"
+    } else {
+      location.href = "/questions/"
+    }
   }, function(error) {
     alert(error.message)
   })
