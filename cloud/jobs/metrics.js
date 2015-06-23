e0ec0290f4a195786d76c7bf417eac2c44d69cfe
Brian Vallelunga <vallelungabrian@gmail.com>
2015-06-22 20:38:13 -0700

    fixed metrics

--- cloud/jobs/metrics.js
+++ cloud/jobs/metrics.js
@@ -8,8 +8,7 @@ Parse.Cloud.job("metrics", function(req, res) {
     "ahq": {},
     "adq": {},
     "maua": null,
-    "mau": null,
-    "uqc": [0, 0, 0, 0]
+    "mau": null
   }
 
   promise.then(function() {
@@ -42,46 +41,26 @@ Parse.Cloud.job("metrics", function(req, res) {
     })
   }).then(function() {
     var queryInstallation = new Parse.Query(Installation)
-    var queryAssignment = new Parse.Query(Assignment)
     var date = new Date()
-    var users = {}
 
     date.setMonth(date.getMonth() - 1)
 
     queryInstallation.matches("timeZone", new RegExp("(America)|(US)", "ig"))
-    queryAssignment.matchesKeyInQuery("creator", "user", queryInstallation)
+    queryInstallation.greaterThanOrEqualTo("updatedAt", date)
 
-    queryAssignment.greaterThanOrEqualTo("createdAt", date)
-
-    return queryAssignment.each(function(question) {
-      users[question.get("creator").objectId] = true
-    }).then(function() {
-      data["maua"] = Object.keys(users).length
+    return queryInstallation.count(function(count) {
+      data["maua"] = count
     })
   }).then(function() {
-    var queryAssignment = new Parse.Query(Assignment)
+    var queryInstallation = new Parse.Query(Installation)
     var date = new Date()
-    var users = {}
 
     date.setMonth(date.getMonth() - 1)
-    queryAssignment.greaterThanOrEqualTo("createdAt", date)
 
-    return queryAssignment.each(function(question) {
-      users[question.get("creator").id] = true
-    }).then(function() {
-      data["mau"] = Object.keys(users).length
-    })
-  }).then(function() {
-    var query = new Parse.Query(User)
+    queryInstallation.greaterThanOrEqualTo("updatedAt", date)
 
-    return query.each(function(user) {
-      var queryAssignment = new Parse.Query(Assignment)
-
-      queryAssignment.equalTo("creator", user)
-
-      return queryAssignment.count(function(count) {
-        data["uqc"][Math.min(count, 3)]++
-      })
+    return queryInstallation.count(function(count) {
+      data["mau"] = count
     })
   }).then(function() {
     console.log(data)
