a66ae8f7ee1a287d6b3db6b27c5c185ff2886890
Brian Vallelunga <vallelungabrian@gmail.com>
2015-06-23 14:31:16 -0700

    fixed tutor upgrades

--- cloud/beforeSave/tutor.js
+++ cloud/beforeSave/tutor.js
@@ -4,11 +4,12 @@ Parse.Cloud.beforeSave("Tutor", function(req, res) {
   var tutor = req.object
 
   Settings().then(function(settings) {
+    if(!tutor.get("rating")) tutor.set("rating", 0)
+    if(!tutor.get("earned")) tutor.set("earned", 0)
+    if(!tutor.get("paid")) tutor.set("paid", 0)
+
     if(!tutor.isNew()) return
 
-    tutor.set("earned", 0)
-    tutor.set("paid", 0)
-    tutor.set("rating", 0)
     tutor.set("question", settings.get("tutorQuestion"))
     tutor.set("enabled", !!tutor.get("enabled"))
     tutor.set("email", !!tutor.get("email"))
