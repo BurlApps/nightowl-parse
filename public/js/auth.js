7c2d00ef45fdc02ec252ff9df5c76218d50218bb
Brian Vallelunga <vallelungabrian@gmail.com>
2015-06-27 02:50:15 -0700

    fixed mixpanel

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,31 +1,23 @@
 $(function() {
   var enablePosting = true
-  
+
   $(".form").on("submit", function(e) {
     e.preventDefault()
     e.stopPropagation()
-    
+
     if(enablePosting) {
       var form = $(this)
       var button = form.find(".submit").val("sending...")
+      var action = form.attr("action")
       enablePosting = false
-      
-      $.post(form.attr("action"), form.serialize(), function(response) {
+
+      $.post(action, form.serialize(), function(response) {
         button.toggleClass("error", !response.success)
 	      button.toggleClass("active", response.success)
 
         if(response.success) {
 	        button.val("awesome :)")
-          
-          if(response.user) {
-            mixpanel.alias(response.user, mixpanel.get_distinct_id())
-            mixpanel.people.set({
-              ID: response.user,
-              $name: response.name,
-              $email: response.email,
-            })
-          }
-          
+
 	      	setTimeout(function() {
             window.location.href = response.next
           }, 300)
